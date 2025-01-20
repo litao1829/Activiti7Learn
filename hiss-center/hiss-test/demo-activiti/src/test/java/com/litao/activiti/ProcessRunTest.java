84657d1a9ea2be6ee6a31a438cefdb8f3181709e
litao <2532566074@qq.com>
2025-01-20 21:37:53 +0800

    保存流程图，部署

---
+++ hiss-center/hiss-test/demo-activiti/src/test/java/com/litao/activiti/ProcessRunTest.java
@@ -0,0 +1,54 @@
+package com.litao.activiti;
+
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.DeploymentBuilder;
+import org.activiti.engine.repository.Model;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProcessRunTest {
+    @Autowired
+    RepositoryService repositoryService;
+
+    @Autowired
+    RuntimeService runtimeService;
+
+    /**
+     * 流程部署测试
+     */
+    @Test
+    public void testDeployment(){
+        String modelId = "604a87d6-d72a-11ef-bc6c-005056c00001";
+        String xmlName = "20.bpmn20.xml";
+        //查询模型基本信息
+        Model model = repositoryService.getModel(modelId);
+        //读取模型的xml内容
+        byte[] xmlContent = repositoryService.getModelEditorSource(modelId);
+        DeploymentBuilder deploymentBuilder = repositoryService.createDeployment()
+                .category(model.getCategory())
+                .name(model.getName())
+                .addBytes(xmlName, xmlContent);
+
+        //部署（把原模型文件复制一份）
+        Deployment deployment = deploymentBuilder.deploy();
+
+        //把部署的ID回显到模型表
+        model.setDeploymentId(deployment.getId());
+        repositoryService.saveModel(model);
+    }
+
+    /**
+     * 运行测试
+     */
+    @ParameterizedTest
+    @ValueSource(strings = {"流程定义key"})
+    public void testRun(String key){
+
+    }
+}
--- hiss-center/hiss-test/demo-activiti/src/test/java/com/litao/activiti/ProcessSaveTest.java
+++ hiss-center/hiss-test/demo-activiti/src/test/java/com/litao/activiti/ProcessSaveTest.java
@@ -1,15 +1,26 @@
 package com.litao.activiti;
 
+import cn.hutool.core.io.FileUtil;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.Model;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class ProcessSaveTest {
+    @Autowired
+    RepositoryService repositoryService;
 
     @ParameterizedTest
     @ValueSource(strings = {"C:\\Users\\Acer\\Desktop\\bpmn-01.bpmn.xml"})
     public void testSaveFromFile(String file){
-
+        Model model = repositoryService.newModel();//快速创建一个流程模型的对象
+        model.setName("单人审批请假流程");//名称
+        model.setCategory("学习");//分类
+        repositoryService.saveModel(model);//保存bpmn设计好的流程文件的方法
+        byte[] bytes = FileUtil.readBytes(file);
+        repositoryService.addModelEditorSource(model.getId(),bytes);//保存流程图的xml文件内容到数据库
     }
 }
